d9834b047e117246ad00b674dc3f1d49af38f949
Afaq Ahmad <you@example.com>
2023-05-17 17:41:10 +0500

    routes added

---
+++ src/routes/public-route/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HomePage } from '../../pages';
+
+const authenticatedRoutes = [
+	{
+		key: 0,
+		path: '/',
+		element: HomePage,
+	},
+];
+
+function withNavigationWatcher(Component, path) {
+	const WrappedComponent = function (props) {
+		return <Component {...props} />;
+	};
+	return <WrappedComponent />;
+}
+export const publicRoutes = authenticatedRoutes.map((route) => {
+	return {
+		...route,
+		element: withNavigationWatcher(route.element, route.path),
+	};
+});
